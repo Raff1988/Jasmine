3f694e06bb7650087d57db0fcbc4741e2cc6cc45
Raff1988 <raffaquat321@hotmail.co.uk>
2020-06-05 10:12:44 +0000

    message

--- Jasmine-Testing/scripts/calc.js
+++ Jasmine-Testing/scripts/calc.js
@@ -1,3 +1,12 @@
-function addtion(numOne, numTwo) {
-    return numOne + numTwo;
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("Error!");
+ 
+    }
 }
